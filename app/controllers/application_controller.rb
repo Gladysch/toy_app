844469f1db65832aa119d690771ae0528bb1501c
GladysCh <gchecasaca@lexuseditores.com>
2017-06-02 13:54:38 -0500

    Adicionando hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+  	render html: "Hello, World!!! I'm Gladys and happy to be learning Rails"
+  end
 end
